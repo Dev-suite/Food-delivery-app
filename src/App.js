42b3e7a20c657a3fb29054ca4385f8c5b2218736
dev-suite <kamaldeensulyman99@gmail.com>
2022-07-22 16:28:43 +0100

    More features coming soon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,173 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useState } from "react";
+import Header from "./Components/Header";
+import MenuContainer from "./Components/MenuContainer";
+import {
+  AccountBalanceWalletRounded,
+  Chat,
+  Favorite,
+  HomeRounded,
+  Settings,
+  SummarizeRounded,
+} from "@mui/icons-material";
+import BannerName from "./Components/BannerName";
+import MenuCard from "./Components/MenuCard";
+import { MenuItems, Items } from "./Components/Data";
+import ItemCard from "./Components/ItemCard";
+import DebitCard from "./Components/DebitCard";
+import SubMenuContainer from "./Components/SubMenuContainer";
+import CartItem from "./Components/CartItem";
+import { useStateValue } from "./Components/StateProvider";
 
 function App() {
+  const [isMainData, setMainData] = useState(
+    Items.filter((element) => element.itemId === "buger01")
+  );
+
+  const [{ cart, total }, dispatch] = useStateValue();
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    const menuLi = document.querySelectorAll("#menu li");
+
+    function setMenuActive() {
+      menuLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    menuLi.forEach((n) => n.addEventListener("click", setMenuActive));
+
+    // menu Card active class changer
+    const menuCard = document
+      .querySelector(".rowContainer")
+      .querySelectorAll(".rowMenuCard");
+
+    function setMenuCardActive() {
+      menuCard.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    menuCard.forEach((n) => n.addEventListener("click", setMenuCardActive));
+  }, [isMainData, cart, total, totalPrice]);
+
+  const setData = (itemId) => {
+    setMainData(Items.filter((element) => element.itemId === itemId));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header section */}
+      <Header />
+
+      {/* Left menu */}
+      <div className="leftMenu">
+        <ul id="menu">
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<HomeRounded />}  isHome/>
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<Chat />}  />
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<AccountBalanceWalletRounded />}  />
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<Favorite />} />
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<SummarizeRounded />}  />
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<Settings />}  />
+          <div className="indicator"></div>
+        </ul>
+      </div>
+      <main>
+        <div className="mainContainer">
+          {/* Banner  */}
+          <div className="banner">
+            <BannerName name={"Devsuite"} discount={"20"} more={"#"} />
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdelivery.png?alt=media&token=69b9823d-96df-452a-bd4e-14d27a4cc337"
+              alt=""
+              className="deliveryPic"
+            />
+          </div>
+
+          <div className="dishContainer">
+            <div className="menuCard">
+              <SubMenuContainer />
+            </div>
+
+            <div className="rowContainer">
+              {MenuItems &&
+                MenuItems.map((data) => (
+                  <div key={data.id} onClick={() => setData(data.itemId)}>
+                    <MenuCard
+                      imgSrc={data.imgSrc}
+                      name={data.name}
+                      isActive={data.id === "1" ? true : false}
+                    />
+                  </div>
+                ))}
+            </div>
+
+            <div className="dishItemContainer">
+              {isMainData &&
+                isMainData.map((data) => (
+                  <ItemCard
+                    key={data.id}
+                    itemId={data.id}
+                    imgSrc={data.imgSrc}
+                    name={data.name}
+                    ratings={data.ratings}
+                    price={data.price}
+                  />
+                ))}
+            </div>
+          </div>
+        </div>
+        <div className="rightMenu">
+          <div className="debitCardContainer">
+            <div className="debitCard">
+              <DebitCard />
+            </div>
+          </div>
+
+          {!cart ? (
+            <div className="addSomeItem">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2FemptyCart.png?alt=media&token=50b733d4-cdd9-4025-bffe-8efa4066ca24"
+                alt=""
+                className="emptyCart"
+              />
+            </div>
+          ) : (
+            <div className="cartCheckOutContianer">
+              <div className="cartContainer">
+                <SubMenuContainer />
+
+                <div className="cartItems">
+                  {cart &&
+                    cart.map((data) => (
+                      <CartItem
+                        key={data.id}
+                        itemId={data.id}
+                        name={data.name}
+                        imgSrc={data.imgSrc}
+                        qty={"4"}
+                        price={data.price}
+                      />
+                    ))}
+                </div>
+              </div>
+              <div className="totalSection">
+                <h3>Total</h3>
+                <p>
+                  <span>$ </span> {total}
+                </p>
+              </div>
+              <button className="checkOut">Check Out</button>
+            </div>
+          )}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,56 @@
+import {
+  BarChart,
+  SearchRounded,
+  ShoppingCartRounded,
+} from "@mui/icons-material";
+import React, { useEffect } from "react";
+import { useStateValue } from "./StateProvider";
+
+function Header() {
+  const [{ cart }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const toggleIcon = document.querySelector(".toggleMenu");
+    toggleIcon.addEventListener("click", () => {
+      document.querySelector(".rightMenu").classList.toggle("active");
+    });
+  }, []);
+
+  return (
+    <header>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Flogo.png?alt=media&token=fc228623-ef27-4af4-8ea5-b9ebeeaf47dc"
+        alt=""
+        className="logo"
+      />
+
+      <div className="inputBox">
+        <SearchRounded className="searchIcon" />
+        <input type="text" placeholder="Search" />
+      </div>
+
+      <div className="shoppingCart">
+        <ShoppingCartRounded className="cart" />
+        <div className={`${!cart ? "noCartItem" : "cart_content"}`}>
+          <p>{cart ? cart.length : ""}</p>
+        </div>
+      </div>
+
+      <div className="profileContainer">
+        <div className="imgBox">
+          <img
+            src="https://pbs.twimg.com/profile_images/1495803246753026053/LMcNpAz0_400x400.jpg"
+            alt=""
+          />
+        </div>
+        <h2 className="userName">Devsuite</h2>
+      </div>
+
+      <div className="toggleMenu">
+        <BarChart className="toggleIcon" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
